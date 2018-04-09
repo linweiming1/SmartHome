9d98cea1d810c78a537d738cd4775f1dfe2cd156
linweiming <linweimingFZ@163.com>
2018-04-09 16:27:40 +0800

    完成app和硬件通讯的代码

--- src/test/java/com/lwm/test/MailTest.java
+++ src/test/java/com/lwm/test/MailTest.java
@@ -3,6 +3,7 @@ package com.lwm.test;
 import com.lwm.MailUtil.EmailUtil;
 import com.lwm.MailUtil.MailSenderInfo;
 import com.lwm.smarthome.controller.VerifyCodeController;
+import com.lwm.util.ToolUtils;
 import com.lwm.util.VerifyCodeUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,16 +14,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:applicationContext.xml")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class MailTest {
-    @Autowired
-    MailSenderInfo mailInfo;
+  //  @Autowired
+  //  MailSenderInfo mailInfo;
 
     private static Logger logger= LoggerFactory.getLogger(MailTest.class);
     @Test
     public void sendEmail() {
-        EmailUtil.sendEmail(mailInfo, "linweimingfz@163.com", "1314");
+      //  EmailUtil.sendEmail(mailInfo, "linweimingfz@163.com", "1314");
      //   System.out.println(VerifyCodeUtils.generateVerifyCode(6,"123456789"));
+        byte[] abc=ToolUtils.stringToByte("[1,2,3]");
+        System.out.println(abc[0]);
     }
 }
